ca47942d6212faa0cf4e92068bd32408a6f35d39
Gabryelle Duarte <gabryelleddev@gmail.com>
2025-03-21 11:28:20 -0300

    feat: add exercícios de Revisão

---
+++ Material Complementar/Exercícios de Revisão (Arrays)/ArrayOrdemInversa.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ArrayOrdemInversa{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        System.out.println("Insira o tamanho do vetor:");
+        int numero= sc.nextInt();
+        int[] array =new int[numero];
+        for (int i=(array.length-1);i>=0;i--){
+            System.out.println("Insira o valor do index "+ i );
+            array[i]=sc.nextInt();
+        }
+        for(int nmr:array){
+            System.out.println(nmr);
+        }
+        
+        sc.close();
+    }
+}
---
+++ Material Complementar/Exercícios de Revisão (Arrays)/ArrayQuadrados.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ArrayQuadrados {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        System.out.println("Insira numero:");
+        int nmr=sc.nextInt();
+        int[] vet = new int [nmr];
+        for(int i=0;i<vet.length;i++){
+            vet[i]=(int) Math.pow((i+1),2);
+            System.out.println(vet[i]);
+        }
+        
+        sc.close();
+    }
+}
